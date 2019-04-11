fab9c24674011d9a07f56a9ba92f41151623b7cc
Puripant <puripant@gmail.com>
2019-04-11 21:11:29 +0700

    Add one more color picker Flatten folder structure

--- app.js
+++ app.js
@@ -30,29 +30,20 @@ export const INITIAL_VIEW_STATE = {
   bearing: 0
 };
 
-const white = [255, 255, 255, 100];
-function interpolateToWhite(start, num, reversed) {
+function interpolateColorPair(start, end, num) {
   let result = [];
-  for (let i = 0; i < num; i++) {
+  for(let i = 0; i < num; i++) {
     let color = [];
-    for (let j = 0; j < start.length; j++) {
-      color.push((start[j] * (num - i - 1) / (num - 1)) + (white[j] * i / (num - 1)));
+    for(let j = 0; j < start.length; j++) {
+      color.push((start[j] * (num - i - 1) / (num - 1)) + (end[j] * i / (num - 1)));
     }
     result.push(color);
   }
-  return reversed ? result.reverse() : result;
+  return result;
 }
-function interpolateColors(start, end, num) { // start & end are RGBA arrays
-  return interpolateToWhite(start, num/2, false).concat(interpolateToWhite(end, num/2, true));
-  // let result = [];
-  // for(let i = 0; i < num; i++) {
-  //   let color = [];
-  //   for(let j = 0; j < start.length; j++) {
-  //     color.push((start[j] * (num - i - 1) / (num - 1)) + (end[j] * i / (num - 1)));
-  //   }
-  //   result.push(color);
-  // }
-  // return result;
+function interpolateColors(start, mid, end, num) { // start, mid, and end are RGBA arrays
+  // TODO currently assume num is an even number
+  return interpolateColorPair(start, mid, num/2).concat(interpolateColorPair(mid, end, num/2));
 }
 
 export class App extends Component {
@@ -74,12 +65,14 @@ export class App extends Component {
       switch (event.target.id) {
         case 'picker0': this.setState({ displayColorPicker0: !this.state.displayColorPicker0 }); break;
         case 'picker1': this.setState({ displayColorPicker1: !this.state.displayColorPicker1 }); break;
+        case 'picker2': this.setState({ displayColorPicker2: !this.state.displayColorPicker2 }); break;
       }
     };
     this.handleClose = () => {
       this.setState({
         displayColorPicker0: false,
         displayColorPicker1: false,
+        displayColorPicker2: false,
       });
     };
 
@@ -89,6 +82,9 @@ export class App extends Component {
     this.handleChange1 = (color) => {
       this.setState({ color1: color.rgb });
     };
+    this.handleChange2 = (color) => {
+      this.setState({ color2: color.rgb });
+    };
 
     this.state = {
       visibleGrid: true,
@@ -97,7 +93,9 @@ export class App extends Component {
       displayColorPicker0: false,
       color0: { r: 0, g: 112, b: 19, a: 1 },
       displayColorPicker1: false,
-      color1: { r: 241, g: 112, b: 19, a: 1 },
+      color1: { r: 255, g: 255, b: 255, a: 0.2 },
+      displayColorPicker2: false,
+      color2: { r: 241, g: 112, b: 19, a: 1 },
       tooltipText: '',
     };
   }
@@ -117,13 +115,15 @@ export class App extends Component {
 
   _renderLayers() {
     const {
-      iconMapping = 'data/location-icon-mapping.json',
-      iconAtlas = 'data/location-icon-atlas.png',
+      iconMapping = 'location-icon-mapping.json',
+      iconAtlas = 'location-icon-atlas.png',
     } = this.props;
     let color0 = Object.values(this.state.color0);
     color0[3] *= 255;
     let color1 = Object.values(this.state.color1);
     color1[3] *= 255;
+    let color2 = Object.values(this.state.color2);
+    color2[3] *= 255;
 
     return [
       new GridLayer({
@@ -134,7 +134,7 @@ export class App extends Component {
         autoHighlight: true,
         cellSize: this.state.gridCellSize,
         extruded: false,
-        colorRange: interpolateColors(color0, color1, 8),
+        colorRange: interpolateColors(color0, color1, color2, 6),
         getPosition: d => d,
         onHover: d => {
           this.setState({
@@ -190,6 +190,9 @@ export class App extends Component {
         color1: {
           background: `rgba(${this.state.color1.r}, ${this.state.color1.g}, ${this.state.color1.b}, ${this.state.color1.a})`,
         },
+        color2: {
+          background: `rgba(${this.state.color2.r}, ${this.state.color2.g}, ${this.state.color2.b}, ${this.state.color2.a})`,
+        },
       },
     });
 
@@ -205,6 +208,7 @@ export class App extends Component {
           <div>
             {this._renderColorPicker(0, styles.color0)}
             {this._renderColorPicker(1, styles.color1)}
+            {this._renderColorPicker(2, styles.color2)}
           </div>
           <div>
             Current value: {this.state.tooltipText ? this.state.tooltipText : ''}
